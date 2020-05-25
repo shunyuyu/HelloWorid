63cabf3c8ea5e7e52d279afd51ecc59ee48400d6
xushunyu <2631582891@qq.com>
2020-05-25 15:10:34 +0800

    登录功能：传递从MainActivity传递手机号码给HomeActivity

--- app/src/main/java/com/example/helloworid/HomeActivity.java
+++ app/src/main/java/com/example/helloworid/HomeActivity.java
@@ -2,13 +2,23 @@ package com.example.helloworid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.helloworid.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private ActivityHomeBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        mBinding=ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        Intent intent = getIntent();
+        String phone = intent.getStringExtra("data_phone");
+        mBinding.textPhone.setText(phone);
+
     }
 }
--- app/src/main/java/com/example/helloworid/MainActivity.java
+++ app/src/main/java/com/example/helloworid/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
                 String pwd = mBinding.editPwd.getText().toString();
                 if(phone.equals("10086")&&pwd.equals("123456")){
                     Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                    intent.putExtra("data_phone",phone);
                     startActivity(intent);
                 }
                 else {
