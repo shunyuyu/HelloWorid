ab2dd9204d04c445595f6d4d2ccb8ee78743d6f9
xushunyu <2631582891@qq.com>
2020-05-25 14:47:25 +0800

    实现功能：点击登录按钮跳转到HomeActivity

--- app/src/main/java/com/example/helloworid/MainActivity.java
+++ app/src/main/java/com/example/helloworid/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.helloworid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.helloworid.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
