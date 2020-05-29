50623be0b3e681397bed1c30b479cff19db8216d
xushunyu <2631582891@qq.com>
2020-05-29 11:14:39 +0800

    实现功能：将用户资料显示到Home界面

--- app/src/main/java/com/example/helloworid/HomeActivity.java
+++ app/src/main/java/com/example/helloworid/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.helloworid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.example.helloworid.databinding.ActivityHomeBinding;
@@ -20,5 +21,13 @@ public class HomeActivity extends AppCompatActivity {
         String phone = intent.getStringExtra("data_phone");
         mBinding.textPhone.setText(phone);
 
+        SharedPreferences sp = getSharedPreferences("user_info", MODE_PRIVATE);
+        String userName = sp.getString("name_"+phone,"0");
+        String userSex = sp.getString("sex_"+phone,"0");
+        String userSms = sp.getString("sms_"+phone,"0").equals("1")?"接受":"不接受";
+        mBinding.textName.setText(userName);
+        mBinding.textSex.setText(userSex);
+        String temp =mBinding.textSms.getText().toString()+"是否接受推送:"+userSms;
+        mBinding.textSms.setText(temp);
     }
 }
