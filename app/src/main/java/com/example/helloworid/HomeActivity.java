847ad2d77f2ef6b8019c10fb059b47423f19c63b
xushunyu <2631582891@qq.com>
2020-06-05 10:09:39 +0800

    HomeActivity实现功能：快速双击退出键，退出登录

--- app/src/main/java/com/example/helloworid/HomeActivity.java
+++ app/src/main/java/com/example/helloworid/HomeActivity.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.helloworid.databinding.ActivityHomeBinding;
 
+/**
+ * @author xushunyu
+ */
 public class HomeActivity extends AppCompatActivity {
 
     private ActivityHomeBinding mBinding;
+    private long exitTime = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,10 +30,26 @@ public class HomeActivity extends AppCompatActivity {
         SharedPreferences sp = getSharedPreferences("user_info", MODE_PRIVATE);
         String userName = sp.getString("name_"+phone,"0");
         String userSex = sp.getString("sex_"+phone,"0");
-        String userSms = sp.getString("sms_"+phone,"0").equals("1")?"接受":"不接受";
+        String userSms = "1".equals(sp.getString("sms_"+phone,"0"))?"接受":"不接受";
         mBinding.textName.setText(userName);
         mBinding.textSex.setText(userSex);
         String temp =mBinding.textSms.getText().toString()+"是否接受推送:"+userSms;
         mBinding.textSms.setText(temp);
+    }
+    /**
+     *拦截系统返回键
+     */
+    @Override
+    public void onBackPressed() {
+        exit();
+    }
+    private void exit() {
+        long time = 2000;
+        if(System.currentTimeMillis() - exitTime > time) {
+            exitTime = System.currentTimeMillis();
+            Toast.makeText(getApplicationContext(), "快速点击两下，可退出当前账号", Toast.LENGTH_SHORT).show();
+        }else{
+            finish();
+        }
     }
 }
