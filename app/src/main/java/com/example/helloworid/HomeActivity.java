5d210526a800cf270d90c5321fd40ba933a37d7d
xushunyu <2631582891@qq.com>
2020-06-05 11:16:38 +0800

    重构：登录界面跳转至Home界面的传递数据方式改成序列化封装

--- app/src/main/java/com/example/helloworid/HomeActivity.java
+++ app/src/main/java/com/example/helloworid/HomeActivity.java
@@ -9,6 +9,9 @@ import android.widget.Toast;
 
 import com.example.helloworid.databinding.ActivityHomeBinding;
 
+import java.time.Instant;
+import java.util.Objects;
+
 /**
  * @author xushunyu
  */
@@ -20,35 +23,38 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mBinding=ActivityHomeBinding.inflate(getLayoutInflater());
+        mBinding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
 
         Intent intent = getIntent();
-        String phone = intent.getStringExtra("data_phone");
-        mBinding.textPhone.setText(phone);
+        UserInfo info = (UserInfo) intent.getSerializableExtra("userInfo");
+        String phone = Objects.requireNonNull(info).getPhone();
+        String userName = info.getUserName();
+        String userSex = info.getSex();
+        String userSms = info.getSms();
 
+        mBinding.textPhone.setText(phone);
         SharedPreferences sp = getSharedPreferences("user_info", MODE_PRIVATE);
-        String userName = sp.getString("name_"+phone,"0");
-        String userSex = sp.getString("sex_"+phone,"0");
-        String userSms = "1".equals(sp.getString("sms_"+phone,"0"))?"接受":"不接受";
         mBinding.textName.setText(userName);
         mBinding.textSex.setText(userSex);
-        String temp =mBinding.textSms.getText().toString()+"是否接受推送:"+userSms;
+        String temp = mBinding.textSms.getText().toString() + "是否接受推送:" + userSms;
         mBinding.textSms.setText(temp);
     }
+
     /**
-     *拦截系统返回键
+     * 拦截系统返回键
      */
     @Override
     public void onBackPressed() {
         exit();
     }
+
     private void exit() {
         long time = 2000;
-        if(System.currentTimeMillis() - exitTime > time) {
+        if (System.currentTimeMillis() - exitTime > time) {
             exitTime = System.currentTimeMillis();
             Toast.makeText(getApplicationContext(), "快速点击两下，可退出当前账号", Toast.LENGTH_SHORT).show();
-        }else{
+        } else {
             finish();
         }
     }
--- app/src/main/java/com/example/helloworid/MainActivity.java
+++ app/src/main/java/com/example/helloworid/MainActivity.java
@@ -10,6 +10,9 @@ import android.widget.Toast;
 
 import com.example.helloworid.databinding.ActivityMainBinding;
 
+/**
+ * @author xushunyu
+ */
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding mBinding;
@@ -26,11 +29,17 @@ public class MainActivity extends AppCompatActivity {
                 String phone = mBinding.editPhone.getText().toString();
                 String pwd = mBinding.editPwd.getText().toString();
                 SharedPreferences sp = getSharedPreferences("user_info", MODE_PRIVATE);
-                String temp_phone = sp.getString("phone_" + phone, "error");
-                String temp_pwd = sp.getString("pwd_" + phone, "error");
-                if (phone.equals(temp_phone) && pwd.equals(temp_pwd)) {
+                String tempPhone = sp.getString("phone_" + phone, "error");
+                String tempPwd = sp.getString("pwd_" + phone, "error");
+                if (phone.equals(tempPhone) && pwd.equals(tempPwd)) {
+                    Bundle bundle = new Bundle();
+                    String userName = sp.getString("name_"+phone,"0");
+                    String userSex = sp.getString("sex_"+phone,"0");
+                    String userSms = "1".equals(sp.getString("sms_"+phone,"0"))?"接受":"不接受";
+                    UserInfo userInfo = new UserInfo(userName,pwd,userSex,phone,userSms);
+                    bundle.putSerializable("userInfo",userInfo);
                     Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                    intent.putExtra("data_phone", phone);
+                    intent.putExtras(bundle);
                     startActivity(intent);
                 } else {
                     Toast.makeText(MainActivity.this, "手机号或密码错误！", Toast.LENGTH_LONG).show();
