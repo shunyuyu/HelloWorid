26593b51deb2e50af1fc1115602b6340a8eea49b
xushunyu <2631582891@qq.com>
2020-06-12 11:21:49 +0800

    新增了两个Activity：ResetPwdActivity和UpdatePwdActivity 并完成了布局和修改好id

---
+++ app/src/main/java/com/example/helloworid/UpdatePwdActivity.java
@@ -0,0 +1,14 @@
+package com.example.helloworid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdatePwdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_pwd);
+    }
+}
